078baa477cff5bf73951cb4cf828e1622516206b
Zia Sahil <sahilmohammadzia@gamil.com>
2025-10-19 07:25:20 -0700

    remote repo

---
+++ first/file.js
@@ -0,0 +1,12 @@
+
+
+
+// nav toggle
+const links = document.querySelector('.links');
+const navLinks = document.querySelector('.nav-links');
+
+links.addEventListener('click', () => {
+    
+    navLinks.classList.toggle('active');
+});
+let header = document.querySelector('.header');

90efdb74bf177dc1d2340a051b223616a72de159
Zia Sahil <sahilmohammadzia@gamil.com>
2025-10-21 08:51:59 -0700

    form validation

--- form handling and validation/script.js
+++ form handling and validation/script.js
@@ -1,7 +1,22 @@
 const form = document.getElementById("signup");
 form.addEventListener('submit', (e) =>{
     e.preventDefault();
+    //handling form submission
     const name = document.getElementById("fname").value;
     const email = document.getElementById("email").value;
     document.writeln(`Name: ${name}, Email: ${email}`);
+
+
+    // form validation
+    const fname = form.fname.value;
+    const emailadd = form.email.value;
+    if (fname.trim() === "" ) {
+        alert("Please fill in your name.");
+    } else if (emailadd.trim() === "" || !emailadd.includes("@")) {
+        alert("Please fill in a valid email.");
+    } else {
+        alert("Form submitted successfully!");
+    }
+
+    
 });

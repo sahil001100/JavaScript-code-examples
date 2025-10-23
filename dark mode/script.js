8062c4d21e626c61735551e2d852e2772d4f8770
Zia Sahil <sahilmohammadzia@gamil.com>
2025-10-23 08:41:19 +0430

    add dark mode toggle button

---
+++ dark mode/script.js
@@ -0,0 +1,5 @@
+// Dark mode toggle script
+const toggleButton = document.getElementById("toggle");
+toggleButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+});
